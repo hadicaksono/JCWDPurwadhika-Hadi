172817accbdb19442b7283ee1e237397edcf82c6
hadicaksono <hadiw219@gmail.com>
2023-08-31 10:03:43 +0700

    exercie & git

--- 02-Looping/forloop.js
+++
@@ -1,8 +0,0 @@
-// for(let i=0;i<10;i++){
-//     console.log(i);
-// }
-
-for (let i=5; i>0; i--){
-    if(i===3) continue //untuk melanjutkan ke value selanjutnya setelah kondisi
-    console.log(i);
-}
