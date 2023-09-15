5e76636d656e0fc361d4985b7ac926ad7b23c83a
hadicaksono <hadiw219@gmail.com>
2023-09-15 10:05:50 +0700

    day-14

--- day-13/Asynchronous.js
+++
@@ -1,9 +0,0 @@
-//setTimeout(message,3000)
-
-function message(){
-    console.log('Asynchronous is easy!');
-}
-
-console.log('Task 1')
-setTimeout(()=>console.log('Task 2'),3000);
-console.log('Task 3');
