809c0a2fadabf41add9992706d9e648b0ec96f76
hadicaksono <hadiw219@gmail.com>
2023-09-17 15:45:01 +0700

    update commit

--- day-3/day-3.js
+++ day-3/day-3.js
@@ -1,1 +1,19 @@
-console.log('makan');
+// Javascript Object
+const car ={
+    brand :"BMW",
+    model :"M135i xDrive",
+    price : 800000000
+}
+// Object Literal syntax
+let user = {}
+// Object constructor syntax
+let user1 = new Object()
+
+// Properties & Method
+let userO = {
+    name :"David",
+    greet(){
+        console.log("Hello");
+    }
+}
+console.log(userO.name.greet()="Billy");
